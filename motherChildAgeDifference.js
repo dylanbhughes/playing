8c03c22ebc977bcc90d05298cd00db7fa7d96fa4
dylanbhughes <dylanbhughes@gmail.com>
2016-01-27 22:59:23 -0500

    Still working on Chapter 5.

---
+++ motherChildAgeDifference.js
@@ -0,0 +1,22 @@
+// Eloquent JavaScript Chapter 5 Problem 2: Mother-child Age Difference
+
+// Book Provided Function
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+// Your code here.
+
+var children = ancestry.reduce(function(a){
+  return if(a.mother);
+});
+
+
+
+// → 31.2
